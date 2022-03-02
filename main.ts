7773ecadaae91d72108016740a02a1c65dd8f0da
yettcode <100731487+yettcode@users.noreply.github.com>
2022-03-02 09:18:09 -0800

    ye

--- main.ts
+++ main.ts
@@ -1,6 +1,4 @@
-hummingbird.startHummingbird()
-music.setBuiltInSpeakerEnabled(true)
-basic.forever(function () {
+function light2 () {
     if (hummingbird.getSensor(SensorType.Light, ThreePort.Two) > 50) {
         hummingbird.setPositionServo(FourPort.Three, 90)
         basic.pause(100)
@@ -12,7 +10,56 @@ basic.forever(function () {
     } else {
     	
     }
+}
+function KRAKEN () {
+    if (input.soundLevel() > 50) {
+        hummingbird.setTriLED(
+        TwoPort.One,
+        200,
+        0,
+        0
+        )
+        basic.pause(100)
+        hummingbird.setTriLED(
+        TwoPort.One,
+        0,
+        0,
+        0
+        )
+        basic.pause(100)
+        hummingbird.setTriLED(
+        TwoPort.Two,
+        200,
+        0,
+        0
+        )
+        basic.pause(100)
+        hummingbird.setTriLED(
+        TwoPort.Two,
+        0,
+        0,
+        0
+        )
+        basic.pause(100)
+        hummingbird.setLED(ThreePort.One, 100)
+        basic.pause(100)
+        hummingbird.setLED(ThreePort.One, 0)
+        basic.pause(100)
+        hummingbird.setLED(ThreePort.Two, 100)
+        basic.pause(100)
+        hummingbird.setLED(ThreePort.Two, 0)
+        basic.pause(100)
+        hummingbird.setLED(ThreePort.Three, 100)
+        basic.pause(100)
+        hummingbird.setLED(ThreePort.Three, 0)
+    } else {
+    	
+    }
+}
+hummingbird.startHummingbird()
+basic.forever(function () {
+    KRAKEN()
 })
 basic.forever(function () {
-	
+    light2()
 })
