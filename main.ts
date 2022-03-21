99c6c4c6cf2e90554b2a8051c17f6d24a9974941
yettcode <100731487+yettcode@users.noreply.github.com>
2022-03-21 09:31:37 -0700

    yee

--- main.ts
+++ main.ts
@@ -5,28 +5,22 @@ function kraken () {
     0,
     0
     )
-    hummingbird.setTriLED(
-    TwoPort.Two,
-    255,
-    0,
-    0
-    )
-    music.playTone(139, music.beat(BeatFraction.Whole))
-    music.playTone(131, music.beat(BeatFraction.Half))
-    music.playTone(139, music.beat(BeatFraction.Half))
-    music.playTone(131, music.beat(BeatFraction.Whole))
-    music.playTone(139, music.beat(BeatFraction.Whole))
-    music.playTone(139, music.beat(BeatFraction.Whole))
-    music.playTone(139, music.beat(BeatFraction.Whole))
-    music.playTone(131, music.beat(BeatFraction.Half))
-    music.playTone(139, music.beat(BeatFraction.Half))
-    music.playTone(131, music.beat(BeatFraction.Whole))
+    hummingbird.setLED(ThreePort.One, 100)
     for (let index = 0; index < 99999999999 * 9999999999; index++) {
         drive()
     }
 }
 function drive () {
     if (hummingbird.getSensor(SensorType.Distance, ThreePort.One) < 5) {
+        hummingbird.setTriLED(
+        TwoPort.Two,
+        0,
+        0,
+        255
+        )
+        hummingbird.setLED(ThreePort.Two, 100)
+        hummingbird.setLED(ThreePort.Three, 100)
+        KRAKEN()
         hummingbird.setRotationServo(FourPort.One, -100)
         basic.pause(1000)
         hummingbird.setRotationServo(FourPort.One, 100)
@@ -37,7 +31,16 @@ function drive () {
     }
 }
 function KRAKEN () {
-	
+    music.playTone(139, music.beat(BeatFraction.Whole))
+    music.playTone(131, music.beat(BeatFraction.Half))
+    music.playTone(139, music.beat(BeatFraction.Half))
+    music.playTone(131, music.beat(BeatFraction.Whole))
+    music.playTone(139, music.beat(BeatFraction.Whole))
+    music.playTone(139, music.beat(BeatFraction.Whole))
+    music.playTone(139, music.beat(BeatFraction.Whole))
+    music.playTone(131, music.beat(BeatFraction.Half))
+    music.playTone(139, music.beat(BeatFraction.Half))
+    music.playTone(131, music.beat(BeatFraction.Whole))
 }
 function light2 () {
     if (hummingbird.getSensor(SensorType.Light, ThreePort.Three) > 50) {
@@ -50,12 +53,12 @@ function light2 () {
 }
 hummingbird.startHummingbird()
 basic.forever(function () {
+	
+})
+basic.forever(function () {
     if (hummingbird.getSensor(SensorType.Sound, ThreePort.Two) >= 84) {
         kraken()
     }
-})
-basic.forever(function () {
-    KRAKEN()
 })
 basic.forever(function () {
     light2()
